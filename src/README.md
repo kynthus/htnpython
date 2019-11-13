47411f2eff9e7f78bcfcbcbde7b2422192822cd0
Hatano Yuusuke <f1047107@kgu.jp>
2019-11-13 23:06:52 +0900

    Add setuptools
    Add setup.py and README.md

---
+++ src/README.md
@@ -0,0 +1,19 @@
+# HtnHello
+「Hello, World」 for PyPI.
+
+[![Build Status](https://travis-ci.org/takezyou/pydata.svg?branch=master)](https://travis-ci.org/takezyou/pydata)
+[![Coverage Status](https://coveralls.io/repos/github/takezyou/pydata/badge.svg?branch=master)](https://coveralls.io/github/takezyou/pydata?branch=master)
+[![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+
+
+## Installation
+```
+pip install htnhello
+```
+
+## Usage
+```
+usage: htnhello
+
+Printing Hello, World.
+```
---
+++ src/setup.py
@@ -0,0 +1,40 @@
+# -*- coding:utf-8 -*-
+
+r"""
+Setup script for PyPI.
+"""
+
+from codecs import open
+from os import path
+
+from setuptools import setup, find_packages
+
+here = path.abspath(path.dirname(__file__))
+
+with open(
+    filename=path.join(here, 'README.md'),
+    encoding='utf-8',
+) as desc:
+    long_description = desc.read()
+
+setup(
+    name='htnhello',
+    version='1.1.0',
+    description='Hello World for Nexus PyPI.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/kynthus/htnpython',
+    author='Kynthus Aueoau',
+    license='MIT',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'htnhello=htnhello.greet:hello',
+        ],
+    },
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
